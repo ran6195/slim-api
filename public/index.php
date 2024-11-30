db05a83802c9dcbf53d3c37e5263fde9864e71b5
Francesco <ran6195@gmail.com>
2024-11-30 17:33:54 +0100

    GetProduct Middleware

--- public/index.php
+++ public/index.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Middleware\AddJsonResponseHeader;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use DI\ContainerBuilder;
 use Slim\Handlers\Strategies\RequestResponseArgs;
+
 
 
 define('APP_ROOT', dirname(__DIR__));
@@ -31,6 +33,8 @@ $error_middleware = $app->addErrorMiddleware(true, true, true);
 $error_handler = $error_middleware->getDefaultErrorHandler();
 $error_handler->forceContentType('application/json');
 
+$app->add(new AddJsonResponseHeader);
+
 $app->get('/api/products', function (Request $request, Response $response) {
 
     $repository = $this->get(App\Repositories\ProductRepository::class);
@@ -38,7 +42,7 @@ $app->get('/api/products', function (Request $request, Response $response) {
     $data = $repository->getAll();
 
     $response->getBody()->write(json_encode($data));
-    return $response->withHeader('Content-Type', 'application/json');
+    return $response;
 });
 
 $app->get('/api/products/{id:[0-9]+}', function (Request $request, Response $response, string $id) {
@@ -53,6 +57,8 @@ $app->get('/api/products/{id:[0-9]+}', function (Request $request, Response $res
 
     $response->getBody()->write(json_encode($data));
 
-    return $response->withHeader('Content-Type', 'application/json');
+    return $response;
 });
+
+
 $app->run();
